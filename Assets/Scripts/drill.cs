2f98bb117a860af453cb32f35a3977c356947d78
takashiski <takashiskibb@gmail.com>
2014-07-17 03:06:22 +0900

    add gitignore TrialsFolder

---
+++ Assets/Scripts/drill.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class drill : MonoBehaviour {
+	public GameObject driver;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		driver.transform.Rotate (new Vector3(0,120,0));
+	}
+}
